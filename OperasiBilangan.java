365ac864729939e53732254760ec5f558f578ae1
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-05-19 02:21:49 +0700

    Create OperasiBilangan.java

---
+++ OperasiBilangan.java
@@ -0,0 +1,32 @@
+package Praktikum4;
+
+public class OperasiBilangan {
+    protected double a,b,c ;
+    
+    protected void setA(double a){
+        this.a = a;
+    }
+    
+    protected void setB(double b){
+        this.b = b;
+    }
+    
+    protected void setC(){
+
+    }
+    
+    protected double getA(){
+        return a;
+    }
+    
+    protected double getB(){
+        return b;
+    }
+    
+    protected double getC(){
+        return c;
+    }
+    
+    protected void tampil(){
+    } 
+}
