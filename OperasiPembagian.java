b89639e9b913ca16d467f0d253b8637bfb3a41ce
MuhammadRiyoUbaidillah <82527896+MuhammadRiyoUbaidillah@users.noreply.github.com>
2021-05-19 02:25:28 +0700

    Create OperasiPembagian.java

---
+++ OperasiPembagian.java
@@ -0,0 +1,38 @@
+package Praktikum4;
+
+public class OperasiPembagian extends OperasiBilangan{
+     @Override
+    protected void setA(double a){
+        this.a = a;
+    }
+    
+     @Override
+    protected void setB(double b){
+        this.b = b;
+    }
+    
+     @Override
+    protected void setC(){
+        this.c=a/b;
+
+    }
+    
+    @Override
+    protected double getA(){
+        return a;
+    }
+    
+     @Override
+    protected double getB(){
+        return b;
+    }
+    
+     @Override
+    protected double getC(){
+        return c;
+    }
+    
+     @Override
+    protected void tampil(){
+    } 
+}
